b9ceb2bbffbd6e72ee3b7a0ecae20beb84971055
E.Lizhenin <elizhenin@lsbr.ru>
2018-06-24 01:28:11 +0300

    Multitheaded version of server. Another sample site for localhost.

--- App/main.js
+++ App/main.js
@@ -3,32 +3,52 @@ Environment.APPDIR = __dirname + "/";
 Environment.ROOTDIR = Environment.APPDIR + "../";
 Environment.SITESDIR = Environment.ROOTDIR + "sites";
 Environment.listen_port = 5000;
-//common requres
-var express = require('express');
-var path = require('path');
-var fs = require('fs');
+Environment.workers_per_cpu = 4;
 
-Environment.app = express();
-Environment.app
-    .get(/(.+)$/,
-        function(req, res) {
-            domain = req.hostname;
-            console.log('request for "' + domain + '"');
-            var result = '';
-            var requested = path.join(Environment.SITESDIR, domain, req.params[0]);
-            console.log('search for file "' + req.params[0] + '"');
-            var fallback = path.join(Environment.SITESDIR, domain, 'index.html');
-            if (fs.existsSync(requested)) {
-                result = requested;
-            } else if (fs.existsSync(fallback)) {
-                result = fallback;
-            } else result = path.join(Environment.SITESDIR, 'index.html');
-            console.log('sending "' + result + '"');
-            res.sendFile(result);
+var cluster = require('cluster');
+if (cluster.isMaster) { // master process
+    // Count the machine's CPUs
+    var cpuCount = require('os').cpus().length;
+    // Create a workers group for each CPU
+    for (var i = 0; i < cpuCount * Environment.workers_per_cpu; i += 1) {
+        cluster.fork();
+    }
+} else { // worker process
+    var express = require('express');
+    var path = require('path');
+    var fs = require('fs');
+    Environment.app = express();
+    Environment.app
+        .get(/(.+)$/,
+            function(req, res) {
+                domain = req.hostname;
+                console.log('Worker ID "' + cluster.worker.id + '"');
+                console.log('reached request for "' + domain + '"');
+                var result = '';
+                var requested = path.join(Environment.SITESDIR, domain, req.params[0]);
+                console.log('search for file "' + req.params[0] + '"');
+                var fallback = path.join(Environment.SITESDIR, domain, 'index.html');
+                if (fs.existsSync(requested)) {
+                    result = requested;
+                } else if (fs.existsSync(fallback)) {
+                    result = fallback;
+                } else result = path.join(Environment.SITESDIR, 'index.html');
+                console.log('sending "' + result + '"');
+                res.sendFile(result);
+                console.log('Worker ID: ' + cluster.worker.id + ' end of task');
+            });
 
-        });
+    var port = process.env.PORT || Environment.listen_port;
+    Environment.app.listen(port, function() {
+        console.log("Listening on " + port);
+    });
+}
 
-var port = process.env.PORT || Environment.listen_port;
-Environment.app.listen(port, function() {
-    console.log("Listening on " + port);
+// Listen for dying workers
+cluster.on('exit', function(worker) {
+
+    // Replace the dead worker
+    console.log('Worker ' + worker.id + ' died and replaced');
+    cluster.fork();
+
 });
--- README.md
+++ README.md
@@ -1,23 +1,27 @@
 # webserver
 Standalone static webserver on Node.js and Express.js.
 
-The main concept is to serve static files and always show index.html instead of 404 error. This feachure is required for application building where CMS feachures such as uri path routing and dynamic page generating is done on browser side by js application.
+The main concept is to serve static files and always show index.html instead of 404 error. This feachure is required for application building where CMS tasks such as uri path routing and dynamic page generating is done on browser side by js application.
 
 So, the logic is "If static file exist, send it. If not - send /index.html". And "index.html" must look at the URI and do the all dirty work.
 
 # Usage:
 1) start server by node.js executable;
 2) go to localhost:5000 to see the sample site working;
-3) look at "sites" subdirectory to see how it organized;
-4) create directory (or symlink to real place with it) with your domain name at "sites" subdirectory and index.html into it;
+3) look at "sites" subdirectory to see how it organized. The bootstrap template is placed there at this moment, not good example but enougth;
+*3a) Hint: create symlink to "localhost" directory and name it "127.0.0.1". See that this way also works, to open your resource by IP instead of domain;*
+4) create directory with your domain name at "sites" subdirectory and index.html into it;
+*4a) Hint: you can make symlink to real place of directory with your files instead;*
 5) place your other required files there;
-6) access server by your domain name and see your site is working.
+6) access server by your domain name and see your site is working;
+7) configure best number of worker threads by modyfying *Environment.workers_per_cpu* in App/main.js file.
+8) Got it!
 
 If you want to change the default port, it can be done in two ways:
-1) set environment variable "PORT" before starting;
-2) change it in App/main.js.
+1) set environment variable "PORT" before starting node.js;
+2) change *Environment.listen_port* variable in App/main.js.
 
 # TODO
-1) make multithreaded version
-2) move setting to external config
-3) improve documentation
+1) <del>make multithreaded version</del> - Done.
+2) move setting to external config;
+3) improve documentation.
